9c3490eba5c16850a55c54260cd4ca4b1e315faf
Joelle Perez <49221382+JLionPerez@users.noreply.github.com>
2025-03-31 16:02:37 -0700

    Add answer
    .split("") adds separators between each character.
    this function splits on whatever value you put in it
    like spaces.

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    const arr = str.split("")
+    const results = []
+    for (let i = arr.length; i >= 0; i--) {
+        results.push(arr[i])
+    }
+    return results.join("")
 };
 
 // Do not edit below this line
