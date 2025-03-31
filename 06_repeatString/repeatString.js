34b65c95679b14360274ead1008688b5b63fdc2b
Joelle Perez <49221382+JLionPerez@users.noreply.github.com>
2025-03-31 15:39:22 -0700

    Add solution

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,9 +1,16 @@
 const repeatString = function(string, num) {
     const arr = []
-    for(let i = 0; i < num; i++) {
-        arr.push(string)
+    const err = "ERROR"
+    if (num >= 0) {
+        for(let i = 0; i < num; i++) {
+            arr.push(string)
+        }
+        return arr.join("");
     }
-    return arr.join("");
+
+    else {
+        return err;
+    }
 };
 
 // Do not edit below this line
