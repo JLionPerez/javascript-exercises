ff2c39d537cf81ae26df9f995577f0a34682d969
Joelle Perez <49221382+JLionPerez@users.noreply.github.com>
2025-04-03 15:38:05 -0700

    Pass second test for removeFromArray
    Explanation for loop: 'const num' is like 'let i'
    just takes in the value from '...nums' since rest
    parameters are just a list of arguments, essentially
    'num' gets assigned a new element as you go through
    the list 'of nums'.
    
    Explanation for splice(): added the count parameter
    after the 'index' so it lets the function know I only
    want it to remove that '1' value at that index. Else,
    it would've just removed every element after the first
    set index.

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,9 +1,13 @@
-const removeFromArray = function(arr, num) {
+const removeFromArray = function(arr, ...nums) {
     const resultsArr = arr;
-    const index = arr.indexOf(num)
-    resultsArr.splice(index, 1) //added count or else splice removes everything after the index
+    for (const num of nums) {
+        const index = arr.indexOf(num)
+        resultsArr.splice(index, 1) 
+    }
     return resultsArr;
 };
+
+// console.log(removeFromArray([1, 2, 3, 4], 3, 2))
 
 // Do not edit below this line
 module.exports = removeFromArray;
