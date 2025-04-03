5ae731da9076561b8843dcd5b6ae3d719f006ab2
Joelle Perez <49221382+JLionPerez@users.noreply.github.com>
2025-04-03 15:17:11 -0700

    Pass first test for removeFromArray
    Added an index file for testing purposes.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, num) {
+    const resultsArr = arr;
+    const index = arr.indexOf(num)
+    resultsArr.splice(index, 1) //added count or else splice removes everything after the index
+    return resultsArr;
 };
 
 // Do not edit below this line
