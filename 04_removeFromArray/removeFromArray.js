437e085a058619bcc5dfb45cd38f6492cc88f87f
Joelle Perez <49221382+JLionPerez@users.noreply.github.com>
2025-04-04 18:39:13 -0700

    Pass first seven tests
    For each input in the rest parameter (...nums)
    Go through the array starting from index of 0
    If a value matches with the inputted value
    Remove that element from the array
    Reset the index to 0 so it starts at the beginning
    of the new array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,50 +1,17 @@
 const removeFromArray = function(arr, ...nums) {
-    let copyArr = arr;
-    let resultsArr = [];
-    for(const num of nums) {
-        for (let i = 0; i < copyArr.length; i++) {
-            if (num != copyArr[i]) {
-                resultsArr.push(copyArr[i])
+    const resultArr = arr;
+
+    for (const num of nums) {
+        for (let i = 0; i < resultArr.length; i++) {
+            if (resultArr[i] == num) {
+                resultArr.splice(i,1)
+                i = 0 // checks through the new array
             }
         }
     }
-    return resultsArr;
+
+    return resultArr;
 };
 
-
-
-// const index = arr.indexOf(num)
-// if (num !== arr[index]) {
-//     resultsArr.push(arr[index])
-//     console.log(resultsArr)
-//     // resultsArr.splice(index)
-//     // console.log(resultsArr)
-//     console.log("a: " + a++)
-// }
-// else {
-//     resultsArr.splice(index)
-//     console.log(resultsArr)
-//     // resultsArr.push(arr[index])
-//     // console.log(resultsArr)
-//     console.log("b: " + b++)
-// }
-
-//takes in array, and rest param
-// if num is in array remove element
-// array.splice(index of num) --> removes values on and after the element
-// save remaining array 
-// if not save element into different array
-// 
-
-//takes in array, and rest param
-// for each num in ...nums 
-// we want to remove/ignore num
-// and what's left over we want to add that to an empty array
-// return that newly populated array
-
-// console.log("answer: " + removeFromArray([1, 2, 2, 3], 2))
-
-console.log("answer: " + removeFromArray([1, 2, 3, 4], 3, 2))
-
 // Do not edit below this line
-// module.exports = removeFromArray;
+module.exports = removeFromArray;
