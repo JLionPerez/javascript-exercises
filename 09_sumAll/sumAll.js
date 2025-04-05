0371990abb5df37904d44e5beb030cc2648d3718
Joelle Perez <49221382+JLionPerez@users.noreply.github.com>
2025-04-04 20:27:24 -0700

    Pass all tests for sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    let sum = 0;
+    let startVal = a;
+    let endVal = b;
 
+    if ( (startVal > 0 && endVal > 0) && (Number.isInteger(a)) && (Number.isInteger(b)) ) {
+        if ( a > b ) {  // adds from the least to greatest values
+            startVal = b;
+            endVal = a;
+        } 
+
+        for (let i = startVal; i <= endVal; i++) {
+            sum += startVal;
+            startVal++
+        }
+        return sum;
+    }
+    else {
+        return "ERROR"
+    }
 };
 
 // Do not edit below this line
