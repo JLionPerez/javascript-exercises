6538ab9ab7815a4f72a7b08d65ae1ea73e98a538
Joelle Perez <49221382+JLionPerez@users.noreply.github.com>
2025-04-08 15:08:23 -0700

    Add answer for leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,19 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if ( year % 100 === 0 ) {
+        let newYear = year/100
+        if ( newYear % 4 === 0 ) {
+            return true
+        }
+        else {
+            return false
+        }
+    }
+    else if ( year % 4 === 0 ) {
+        return true
+    } 
+    else {
+        return false
+    }
 };
 
 // Do not edit below this line
