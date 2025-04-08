d1dadf8b48c1af237c5a162e5d17ff0a64294525
Joelle Perez <49221382+JLionPerez@users.noreply.github.com>
2025-04-08 15:26:21 -0700

    Add answer for convertToCelsius

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,4 +1,7 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degrees) {
+  let celsius = (degrees - 32) * (5/9)
+  celsius = Math.round(celsius * 10) / 10
+  return celsius
 };
 
 const convertToFahrenheit = function() {
