b0d1803aa579d1ac4e54f3bc969bf0bbaba87871
Joelle Perez <49221382+JLionPerez@users.noreply.github.com>
2025-06-24 14:26:58 -0700

    Add solution to fibonacci problem

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -4,14 +4,17 @@ const fibonacci = function(num) {
     let cur = 0;
     length = parseInt(num) - 2;
 
+    if (num > 0 && num <= 2) { return 1; }
+
     if (parseInt(num) >= 0) {
-        for (let i = 0; i < num; i++) {
+        for (let i = 0; i < length; i++) {
             cur = prePrev + prev
             prePrev = prev
             prev = cur
         }
-        return prePrev;
+        return cur;
     }
+
 
     return "OOPS"
 };
