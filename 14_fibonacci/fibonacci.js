94dc6a02ac4d0907d3b0d15c9476b75234462655
Joelle Perez <49221382+JLionPerez@users.noreply.github.com>
2025-06-23 20:25:21 -0700

    Add fib function + stuck on logic

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let prePrev = 1;
+    let prev = 1;
+    let cur = 0;
+    length = parseInt(num) - 2;
 
+    if (parseInt(num) >= 0) {
+        for (let i = 0; i < num; i++) {
+            cur = prePrev + prev
+            prePrev = prev
+            prev = cur
+        }
+        return prePrev;
+    }
+
+    return "OOPS"
 };
 
 // Do not edit below this line
