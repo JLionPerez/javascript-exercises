1a9efbd77dc8a0966797e5e9bf59a4447471dd0f
Joelle Perez <49221382+JLionPerez@users.noreply.github.com>
2025-06-23 19:14:47 -0700

    Add solution to calculator problem

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,50 @@
-const add = function() {
-	
+const add = function(x, y) {
+  return x + y
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let sum = 0;
+
+  for (let n in arr) {
+    sum += arr[n]
+  }
+  
+  return sum
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  let product = 0;
 
+  for (let n in arr) {
+    product = (product * arr[n])
+    if (product === 0) {
+      product = arr[n];
+    }
+  }
+
+  return product
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return Math.pow(x, y)
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let product = 1;
+  let inc = 0;
+
+  if (num === 0 ) { return 1 }  
+
+	for(let i = 0; i < num; i++) {
+    inc++;
+    product = product * inc
+  }
+
+  return product
 };
 
 // Do not edit below this line
