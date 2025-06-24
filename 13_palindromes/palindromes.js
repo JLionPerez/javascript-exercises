37fd122103373c41d9efc7f4030005189215fdb5
Joelle Perez <49221382+JLionPerez@users.noreply.github.com>
2025-06-23 19:51:55 -0700

    Add solution to palindromes problem

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    str = str.replace(/[^a-zA-Z0-9]/gi, '').toLowerCase()
+    let cpyStr = str.split("").reverse().join("");
 
+    if (cpyStr === str) { return true }
+    return false
 };
 
 // Do not edit below this line
