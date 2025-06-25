62f1c7bdc959b916771be69fcbda813fa3d26f4b
Joelle Perez <49221382+JLionPerez@users.noreply.github.com>
2025-06-24 19:15:38 -0700

    Add solution to finding the oldest problem

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let ages = [];
+    let greatAge;
+    const d = new Date();
+    let year = d.getFullYear();
+    let index;
+    for ( let i = 0; i < people.length; i++ ) {
+        if ( !('yearOfDeath' in people[i]) ) {
+            ages.push( year - people[i].yearOfBirth )
+        }
+        else {
+            ages.push(people[i].yearOfDeath - people[i].yearOfBirth)
+        }
+    }
+    greatAge = Math.max(...ages)
+    index = ages.indexOf(greatAge)
+    return people[index]
 };
 
 // Do not edit below this line
